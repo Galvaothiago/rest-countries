9ab9c462f28f8dbcc42fc31b77b3a8b16390b376
thiago <thiagosilva128@gmail.com>
2021-07-31 12:03:44 -0300

    added components to handle with inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { Header } from "./components/Header";
+import styled from 'styled-components'
+import { Filter } from "./components/FilterInput";
+import { Select } from "./components/FilterSelect";
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <Header />
+      <Dashboard>
+        <SearchContainer>
+          <Filter />
+          <Select />
+        </SearchContainer>
+
+      </Dashboard>
+    </>
   );
 }
+
+const Dashboard = styled.main`
+  max-width: 1440px;
+  width: 100%;
+
+  padding: 2.5rem 4rem;
+
+  min-height: calc(100vh - 4rem);
+  height: 100%;
+  background: var(--VeryDarkBlueBackground);
+
+`
+
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
 export default App;
